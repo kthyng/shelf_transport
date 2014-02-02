a7c9748200f31e27af0b70f0f4a762f93e8b4add
Kristen Thyng <kthyng@gmail.com>
2014-02-02 07:12:56 -0600

    fixing up plots scripts on hafen

--- make_plots.py
+++ make_plots.py
@@ -14,8 +14,8 @@ import init
 from datetime import datetime, timedelta
 import glob
 
-matplotlib.rcParams['text.usetex'] = True
-matplotlib.rcParams.update({'font.size': 26})
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams.update({'font.size': 26})
 
 # Tracks to plot
 File = 'tracks/2010-07-01T00gc.nc'
@@ -27,15 +27,15 @@ yg = d.variables['yg'][:]
 tg = d.variables['tp'][:]
 d.close()
 
+# Grid info
+loc = 'http://barataria.tamu.edu:8080/thredds/dodsC/NcML/txla_nesting6.nc'
+grid = tracpy.inout.readgrid(loc)
+
 # Change to projected drifter locations now
 nanind = np.isnan(xg) # indices where nans are location in xg, yg; for reinstitution of nans
 xp, yp, _ = tracpy.tools.interpolate2d(xg, yg, grid, 'm_ij2xy') 
 xp[nanind] = np.nan; yp[nanind] = np.nan
 del(xg,yg) # don't need grid info anymore
-
-# Grid info
-loc = 'http://barataria.tamu.edu:8080/thredds/dodsC/NcML/txla_nesting6.nc'
-grid = tracpy.inout.readgrid(grid)
 
 # Loop through time
 for tind in xrange(2):#tg.size):
@@ -45,6 +45,6 @@ for tind in xrange(2):#tg.size):
 	ax = fig.add_subplot(111)
 	tracpy.plotting.background(grid=grid, ax=ax)
 
-	ax.plot(xg[:,tind], yg[:,tind], 'o', color='darkcyan')
+	ax.plot(xp[:,tind], yp[:,tind], 'o', color='darkcyan')
 	plt.savefig('figures/2010-07-01T00/' + str(tind) + '.png', dpi=150)
-	close(gca)
+	plt.close(fig)
