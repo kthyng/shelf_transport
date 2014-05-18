a833265a2ca199286df90572c84c85f756745f9c
Kristen M. Thyng <kthyng@gmail.com>
2014-05-18 09:03:03 -0500

    D2 in bins

--- make_plots.py
+++ make_plots.py
@@ -422,7 +422,8 @@ def run():
                 else:
                     d = np.load(sfile)
                     metric_temp = d['D2']; nnanstemp = d['nnans']
-                H[i,:] = H[i,:] + metric_temp[:,:,-1]*nnanstemp[:,:,-1] # need to un-average before combining
+                H[i,:] = np.nansum( np.vstack((H[i,:],metric_temp[:,:,-1]*nnanstemp[:,:,-1]))) # need to un-average before combining
+                # H[i,:] = H[i,:] + metric_temp[:,:,-1]*nnanstemp[:,:,-1] # need to un-average before combining
                 nnans[i,:] = nnans[i,:] + nnanstemp[:,:,-1] # need to un-average before combining
 
         # Calculate overall histogram
