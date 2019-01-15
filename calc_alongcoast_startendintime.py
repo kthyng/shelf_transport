727b826f55725ee0e32cd9f85f6d572b64a78e79
Kristen Thyng <kthyng@gmail.com>
2019-01-14 18:43:27 -0600

    now can look at alongcoast conn numbers by simulation start day

--- calc_alongcoast_startendintime.py
+++ calc_alongcoast_startendintime.py
@@ -10,43 +10,66 @@ import pandas as pd
 from glob import glob
 import os
 
+# desired resultant resolution of output
+# '4hours': preserve time resolution across individual simulation to 4 hourly per each sim
+# 'sim': save connectivity by individual simulation
+# 'monthly': (this was done in make_conn_plots.run()) save conn by month-year
+time_res = 'sim'
 
 
 base = 'calcs/alongcoastconn/conn_in_time/'
-lnamest = base + 'lines_by_start.npz'
-lnameen = base + 'lines_by_end.npz'
-# if os.path.exists(lname):
-#     d = np.load(lname)
-#     startingp = d['startingp']; startingn = d['startingn']
-#     endingp = d['endingp']; endingn = d['endingn']; dates = d['dates']; dist = d['dist']
-#
-# else:
+os.makedirs(base, exist_ok=True)
+lnamest = base + 'lines_by_start_%s.npz' % time_res
+lnameen = base + 'lines_by_end_%s.npz' % time_res
 Files = sorted(glob(base + '*.npz'))
 
 t = np.load(Files[0])['t']
 
-startingp = np.empty((len(Files), t.size, 342));
-startingn = np.empty((len(Files), t.size, 342))
-endingp = np.empty((len(Files), t.size, 342));
-endingn = np.empty((len(Files), t.size, 342))
+if time_res == '4hours':
+
+    startingp = np.empty((len(Files), t.size, 342));
+    startingn = np.empty((len(Files), t.size, 342))
+    endingp = np.empty((len(Files), t.size, 342));
+    endingn = np.empty((len(Files), t.size, 342))
+
+elif time_res == 'sim':
+
+    startingp = np.empty((len(Files), 342));
+    startingn = np.empty((len(Files), 342))
+    endingp = np.empty((len(Files), 342));
+    endingn = np.empty((len(Files), 342))
+
+
 dates = []
-i = 0
 
 for i, File in enumerate(Files):
 
-    # print(File)
-    dates.append(pd.Timestamp(File.split('/')[-1][:10]))
-    mat = np.load(File)['mat']
-    # make downcoast negative
-    ix, iy = np.tril_indices(mat.shape[2], k=1)
-    mat[:, ix, iy] = -mat[:, ix, iy]
-    matp = np.ma.masked_where(mat<0, mat)
-    matn = np.ma.masked_where(mat>0, mat)
-    startingp[i,:,:] = matp.sum(axis=2)  # gives upcoast alongcoast conn as function of starting position
-    endingp[i,:,:] = matp.sum(axis=1)
-    startingn[i,:,:] = matn.sum(axis=2)  # gives downcoast alongcoast conn as function of starting position
-    endingn[i,:,:] = matn.sum(axis=1)
-    # i += 1
+    print(File)
+    if time_res == '4hours':
+        dates.append(pd.Timestamp(File.split('/')[-1][:10]))
+        mat = np.load(File)['mat']
+        # make downcoast negative
+        ix, iy = np.tril_indices(mat.shape[2], k=1)
+        mat[:, ix, iy] = -mat[:, ix, iy]
+        matp = np.ma.masked_where(mat<0, mat)
+        matn = np.ma.masked_where(mat>0, mat)
+        startingp[i,:,:] = matp.sum(axis=2)  # gives upcoast alongcoast conn as function of starting position
+        endingp[i,:,:] = matp.sum(axis=1)
+        startingn[i,:,:] = matn.sum(axis=2)  # gives downcoast alongcoast conn as function of starting position
+        endingn[i,:,:] = matn.sum(axis=1)
+    elif time_res == 'sim':
+        # sums over 30 days of simulation time
+        dates.append(pd.Timestamp(File.split('/')[-1][:10]))
+        mat = np.load(File)['mat']
+        # make downcoast negative
+        ix, iy = np.tril_indices(mat.shape[2], k=1)
+        mat[:, ix, iy] = -mat[:, ix, iy]
+        matp = np.ma.masked_where(mat<0, mat)
+        matn = np.ma.masked_where(mat>0, mat)
+        startingp[i,:] = matp.sum(axis=2).sum(axis=0) # gives upcoast alongcoast conn as function of starting position
+        endingp[i,:] = matp.sum(axis=1).sum(axis=0)
+        startingn[i,:] = matn.sum(axis=2).sum(axis=0)  # gives downcoast alongcoast conn as function of starting position
+        endingn[i,:] = matn.sum(axis=1).sum(axis=0)
 
 np.savez(lnamest, t=t, startingp=startingp, startingn=startingn, dates=dates, dist=dist)
 np.savez(lnameen, t=t, endingp=endingp, endingn=endingn, dates=dates, dist=dist)
